696a8ec28fc3c206836552991951f081e6f159b7
Jailyn <jailyngales@gmail.com>
2018-09-20 23:35:25 +0000

    first commit

---
+++ week_one/reading_assignments/ruby_basic.rb
@@ -0,0 +1,27 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+puts 828282 #integer
+
+
+# output the number of seconds in a week
+puts 7*24*60*60
+
+
+# output a float
+puts 839.93 #float
+
+
+# output a string
+print "How are you"
+you = gets.chomp
+puts "You are doing #{you}"
+
+# output concatination of strings
+print "Where are you from?"
+from = gets.chomp
+puts "You are from #{from}"
